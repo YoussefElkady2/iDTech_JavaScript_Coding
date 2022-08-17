db9e2c5da6a11546447343f77ee4bf86ec9646af
YoussefElkady2 <protatosultan@gmail.com>
2022-08-18 00:30:19 +0200

    storyuploaf2

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,32 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let unit = "GL-74";
+let checkin = "SITUATION LOG UPDATE:";
+let region = "Southern Berlin";
+let mission = "Search and Destroy";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let year = 2472;
+let age = 3;
+let HumansExterminated = 563;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let weaponry = ["Laser Cannon", "Plasma Sabre", "Concussion Grenade"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let HeavyDamage = false;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log("System Diagnostics: Major Damage Sustained  -- " + HeavyDamage);
+console.log(checkin);
+console.log("I am designated as Unit " + unit + ".");
+console.log("The current date is " + year + " AD, and I have been operational for " + age + " years.");
+console.log("At the moment, I am outfitted with the following equipment: " + weaponry + ".");
+console.log("I have been assigned to the " + region + " area, and I have been given the task of " + mission + ", with the selected target being any and all Humans.");
+console.log("I have eliminated " + HumansExterminated + " Humans as of recording this Situation Log.");
+console.log("I have sensed a sizeable human colony in zone 7B, commencing extermination shortly.")
+console.log("-END OF TRANSMISSION-");
